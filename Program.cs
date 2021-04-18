286e4d0383594ecb9ac8bbc8a35a498fce3b7abd
Rafael Willen <rafaelpadre20@gmail.com>
2021-04-18 14:17:25 +0100

    Possibilidade de passar o ficheiro como argumento

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace dart_compiler
 {
@@ -6,7 +7,26 @@ namespace dart_compiler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            switch (args.Length)
+            {
+                case < 1:
+                    Console.WriteLine("Erro fatal: Não existe o ficheiro de entrada");
+                    Console.WriteLine("Uso: DartCompiler <caminho_ficheiro>");
+                    return;
+                case > 1:
+                    Console.WriteLine("Demasiados argumentos");
+                    Console.WriteLine("Uso: DartCompiler <caminho_ficheiro>");
+                    return;
+            }
+
+            // Verifica se o ficheiro existe
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine($"Erro Fatal: Não foi possível encontrar o ficheiro {args[0]}");
+                return;
+            }
+
+            Console.WriteLine("All Good!");
         }
     }
 }
