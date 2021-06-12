02d3124a3e76902d5449b6264964f4c2cb10c8b3
Rafael Willen <rafaelpadre20@gmail.com>
2021-06-12 17:25:15 +0100

    Parser can analyse variable declaration

--- Core/Parser/Parser.cs
+++ Core/Parser/Parser.cs
@@ -11,12 +11,12 @@ namespace dart_compiler.Core.Parser
 
         public Parser()
         {
-
+            symbol = TableSymbol.GetNextSymbol();
         }
 
         public void Parse()
         {
-            symbol = TableSymbol.GetNextSymbol();
+            variableDeclaration();
         }
 
         private void readNextSymbol()
@@ -26,9 +26,150 @@ namespace dart_compiler.Core.Parser
 
         private void error(string message)
         {
-            Console.WriteLine($"Erro - encontrado caracter {symbol.Token} - {message}");
+            Console.WriteLine($"Erro - encontrado caracter {symbol.Token} - {message} at line {symbol.LineOfCode}");
             Console.ReadLine();
         }
+
+        #region Var Declaration
+        /// <summary>
+        /// varDeclaration ::= declaredIdentifier (',' IDENTIFIER)*
+        /// </summary>
+        private void variableDeclaration()
+        {
+            declaredIdentifier();
+            if (symbol.Token == Token.TokenEndStatement)
+            {
+                Console.WriteLine("Success");
+                return;
+            }
+            else
+            {
+                error("Expected ';'");
+            }
+            bool loopFalse = true;
+            while (loopFalse)
+            {
+                loopFalse = symbol.Token == Token.TokenComma;
+                if (loopFalse) readNextSymbol();
+                else error("Expected ','");
+                loopFalse = symbol.Token == Token.TokenID;
+                if (loopFalse) readNextSymbol();
+                else error("Expected an identifier");
+                loopFalse = symbol.Token == Token.TokenComma;
+            }
+            Console.WriteLine("Success");
+        }
+
+
+
+        /// <summary>
+        /// declaredIdentifier ::= finalConstVarOrType IDENTIFIER
+        /// </summary>
+        private void declaredIdentifier()
+        {
+            finalConstVarOrType();
+            if (symbol.Token == Token.TokenID)
+            {
+                readNextSymbol();
+            }
+            else
+            {
+                error("Expected an identifier");
+            }
+        }
+
+        /// <summary>
+        /// finalConstVarOrType ::=  ('final' | 'const') type? | ('var' | type)
+        /// </summary>
+        private void finalConstVarOrType()
+        {
+            if (symbol.Token == Token.TokenKeywordFinal || symbol.Token == Token.TokenKeywordConst)
+            {
+                readNextSymbol();
+                if (symbol.Token == Token.TokenID || symbol.isDataType())
+                {
+                    type();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else if (symbol.Token == Token.TokenKeywordVar)
+            {
+                readNextSymbol();
+            }
+            else
+            {
+                type();
+            }
+        }
+
+        /// <summary>
+        /// type ::= (IDENTIFIER | DATA_TYPE) typeArguments?
+        /// </summary>
+        private void type()
+        {
+            if (symbol.Token == Token.TokenID || symbol.isDataType())
+            {
+                readNextSymbol();
+            }
+            else
+            {
+                error("Expected a data type");
+            }
+
+            if (symbol.Token == Token.TokenLess)
+            {
+                typeArguments();
+            }
+        }
+
+        /// <summary>
+        /// typeArguments ::= '<' type (',' type)* '>'
+        /// </summary>
+        private void typeArguments()
+        {
+            if (symbol.Token == Token.TokenLess)
+            {
+                readNextSymbol();
+            }
+            else
+            {
+                error("Expected a '<'");
+            }
+
+            if (symbol.Token == Token.TokenID)
+            {
+                readNextSymbol();
+            }
+            else
+            {
+                error("Expected a identifier");
+            }
+            bool loopFalse = symbol.Token == Token.TokenComma;
+            while (loopFalse)
+            {
+                loopFalse = symbol.Token == Token.TokenComma;
+                if (loopFalse) readNextSymbol();
+                else error("Expected ','");
+                loopFalse = symbol.Token == Token.TokenID;
+                if (loopFalse) readNextSymbol();
+                else error("Expected an identifier");
+                loopFalse = symbol.Token == Token.TokenComma;
+            }
+
+            if (symbol.Token == Token.TokenGreater)
+            {
+                readNextSymbol();
+            }
+            else
+            {
+                error("Expected a '>'");
+            }
+
+        }
+        #endregion
 
         /*
             S --> Az | z
