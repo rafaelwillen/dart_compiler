8d21f170ab05e0a7a7120fbf50c6e7d01e9e071b
Rafael Willen <rafaelpadre20@gmail.com>
2021-07-08 16:10:02 +0100

    Symbols for function and variable for semantics

---
+++ Core/Semantic/Function.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace dart_compiler.Core.Utils
+{
+    public class Function
+    {
+        public string Identifier { get; set; }
+
+        public List<Variable> Arguments { get; set; }
+
+        public string ReturnType { get; set; }
+
+        public bool IsVoid => ReturnType.Equals("void");
+
+        public Function(string identifier, string returnType)
+        {
+            Identifier = identifier;
+            Arguments = new List<Variable>();
+            ReturnType = returnType;
+        }
+    }
+}
---
+++ Core/Semantic/Variable.cs
@@ -0,0 +1,30 @@
+namespace dart_compiler.Core.Utils
+{
+    public class Variable
+    {
+        public string Identifier { get; set; }
+
+        public string DataType { get; set; }
+
+        public string ValueAssigned { get; set; }
+
+        public int Scope { get; set; }
+
+        public Variable(string identifier, string dataType, int scopeLevel)
+        {
+            Identifier = identifier;
+            DataType = dataType;
+            Scope = scopeLevel;
+            ValueAssigned = string.Empty;
+        }
+
+        public Variable(string identifier, string dataType, int scopeLevel, string valueAssigned)
+        {
+            Identifier = identifier;
+            DataType = dataType;
+            Scope = scopeLevel;
+            ValueAssigned = valueAssigned;
+        }
+
+    }
+}
